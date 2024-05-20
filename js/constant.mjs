180385954cf3dff111f9fb97154b14ca61b37235
X7-C <ussrspartanx@gmail.com>
2024-05-20 23:56:28 +0200

    ConsoleLogWorks
    When Pressing Register
    Displays:
    Name,
    Email,
    Password

--- js/constant.mjs
+++ js/constant.mjs
@@ -1,16 +1,16 @@
-const API_BASE_URL = ' https://v2.api.noroff.dev/blog/posts/maxswag';
+// const API_BASE_URL = ' https://v2.api.noroff.dev/blog/posts/maxswag ';
 
-  fetch(API_BASE_URL)
-  .then(response => {
-    if (response.ok) {
-        return response.json();
-    } else {
-        throw new Error("Can't connect to JSON")
-    }
-  })
-  .then (data => {
-    console.log(data);
-  }) 
-  .catch(error => {
-    console.log(error);
-  });
+//   fetch(API_BASE_URL)
+//   .then(response => {
+//     if (response.ok) {
+//         return response.json();
+//     } else {
+//         throw new Error("Can't connect to JSON")
+//     }
+//   })
+//   .then (data => {
+//     console.log(data);
+//   }) 
+//   .catch(error => {
+//     console.log(error);
+//   });
--- js/register.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("registerToIndex").addEventListener("click", function() {
-    window.location.href = "../index.html";
-});
